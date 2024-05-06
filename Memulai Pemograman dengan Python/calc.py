c800ad1e428a194bab993ea2d873dcf030ab4a7e
Evander Siregar <evanderregar@gmail.com>
2024-05-06 14:07:05 +0700

    first commmit

---
+++ Memulai Pemograman dengan Python/calc.py
@@ -0,0 +1,12 @@
+class Calc:
+    def __init__(self, _i):
+        """kalkulator tambah kurang"""
+
+    def __init__(self, _i):
+        self.i = _i
+
+    def tambah(self, _i):
+        return self.i + _i
+
+    def kurang(self, _i):
+        return self.i - _i
